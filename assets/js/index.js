6841d7bf8018ae220365e8a08286309a554bc0db
Grant King <grantryan.king@gmail.com>
2022-11-10 23:31:11 -0800

    Update gallery
    Update ids, classes, styles on image gallery component. Extract inline styles to styles.css , update index.js logic to make it easy to maintain and duplicate the component.

--- assets/js/index.js
+++ assets/js/index.js
@@ -13,13 +13,13 @@ function set_height(element, height_int){
 }
 
 function view_image(event){
-    const main_img = document.getElementById("main-img-col")
     
     //check if img or column, need to set the column bg
     let thumbnail_img = event.target
     if(event.target.tagName == "IMG"){
         thumbnail_img = event.target.parentNode
     }
+    const main_img = thumbnail_img.parentNode.parentNode.parentNode.querySelector('.col')
     main_img.style.background = thumbnail_img.style.background
 }
 
@@ -27,5 +27,8 @@ function view_image(event){
 const page = document.querySelector("body")
 page.addEventListener('wheel', scroll_nav_height)
 
-const thumbnail_gallery = document.getElementById("thumbnail-cols-col")
-thumbnail_gallery.addEventListener('click', view_image)
+//add event listeners for all thumbnail galleries
+const thumbnail_galleries = document.getElementsByClassName("thumbnail-cols-col")
+for(i=0; i<thumbnail_galleries.length; i++){
+    thumbnail_galleries[i].addEventListener('click', view_image)
+}
