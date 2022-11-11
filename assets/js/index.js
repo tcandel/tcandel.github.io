c95bc4148e6bb457068c3fcd7f5d5cc12251f650
Grant King <grantryan.king@gmail.com>
2022-11-10 22:14:36 -0800

    Add image gallery component to main page
    Add JS gallery component to index.html, update index.js. Add image files.
    
    Update secondary top nav.
    
    Update design.

--- assets/js/index.js
+++ assets/js/index.js
@@ -12,5 +12,20 @@ function set_height(element, height_int){
     element.style.height = `${height_int}px`
 }
 
+function view_image(event){
+    const main_img = document.getElementById("main-img-col")
+    
+    //check if img or column, need to set the column bg
+    let thumbnail_img = event.target
+    if(event.target.tagName == "IMG"){
+        thumbnail_img = event.target.parentNode
+    }
+    main_img.style.background = thumbnail_img.style.background
+}
+
+
 const page = document.querySelector("body")
 page.addEventListener('wheel', scroll_nav_height)
+
+const thumbnail_gallery = document.getElementById("thumbnail-cols-col")
+thumbnail_gallery.addEventListener('click', view_image)
